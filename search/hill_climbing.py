a23d1cf30d47c77acf6a2a7f7dc19c75d3f0f16a
Mahdi Zargarzadeh <90433545+zargarzadehmahdi@users.noreply.github.com>
2026-06-03 15:18:13 +0330

    generate neighbors and select best candidate

--- search/hill_climbing.py
+++ search/hill_climbing.py
@@ -1,28 +1,33 @@
 from search.local_search_base import LocalSearchBase
 
-
 class HillClimbing(LocalSearchBase):
-    def run(self, initial_state, **kwargs):
-        """
-        TODO: Implement the Hill Climbing algorithm.
-        
-        Parameters
-        ----------
-        initial_state : list of tuples
-            The initial configuration of sensors.
-        **kwargs : 
-            Define and add any other parameters you might need for the algorithm 
+    def run(self, initial_state):
+        max_iters = 300
+        num_neighbors = 25
 
-        Returns
-        -------
-        best_state : list of tuples
-            The best configuration found.
-        best_cost : int or float
-            The cost of the best configuration.
-        evaluations : list
-            List of costs at each iteration (used for plotting).
-        states_history : list of lists
-            List of states at each iteration (used for animation).
-        """
-        
-        raise NotImplementedError("Students must implement this method.")
+        currentstate = list(initial_state)
+        currentcost = self.evaluate(currentstate)
+
+        for i in range(max_iters):
+            neighbors = []
+
+            for j in range(num_neighbors):
+                neighbors.append(self.get_neighbor(currentstate))
+
+            bestN = neighbors[0]
+            bestNcost = self.evaluate(bestN)
+
+            for k in range(1, len(neighbors)):
+                cost = self.evaluate(neighbors[k])
+
+                if cost < bestNcost:
+                    bestN = neighbors[k]
+                    bestNcost = cost
+
+            if bestNcost < currentcost:
+                currentstate = bestN
+                currentcost = bestNcost
+            else:
+                break
+
+        return currentstate, currentcost
