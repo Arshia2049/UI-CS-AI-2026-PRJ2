51d2e62781b4c98ef1631362df3303f5517f6dc4
mnmgalisafa <mnmgalisafa@gmail.com>
2026-06-03 07:47:36 -0700

    Update simulated_annealing.py

--- search/simulated_annealing.py
+++ search/simulated_annealing.py
@@ -1,24 +1,48 @@
+import math
+import random
 from search.local_search_base import LocalSearchBase
 
-
 class SimulatedAnnealing(LocalSearchBase):
-    def run(self, initial_state, **kwargs):
-        """
-        TODO: Implement the Simulated Annealing algorithm.
-        
-        Parameters
-        ----------
-        initial_state : list of tuples
-            The initial configuration of sensors.
-        **kwargs : 
-            Define and add all necessary parameters required for Simulated Annealing 
+    def run(self, start_state):
+        start_temp = 120.0
+        min_temp = 0.05
+        cooling_rate = 0.98
+        max_loops = 4000
 
-        Returns
-        -------
-        best_state : list of tuples
-        best_cost : int or float
-        evaluations : list
-        states_history : list of lists
-        """
+        working_state = list(start_state)
+        working_cost = self.evaluate(working_state)
+        the_best_state = list(working_state)
+        the_best_cost = working_cost
+        all_evals = []
+        all_states = []
+
+        temp = start_temp
         
-        raise NotImplementedError("Students must implement this method.")
+        for step in range(max_loops):
+            all_evals.append(working_cost)
+            all_states.append(list(working_state))
+            if temp < min_temp:
+                break
+            next_guess = self.get_neighbor(working_state)
+            next_cost = self.evaluate(next_guess)
+            difference = next_cost - working_cost
+            accept_move = False
+            
+            if difference < 0:
+                accept_move = True
+            else:
+                prob = math.exp(-difference / temp)
+                rand_chance = random.random()
+                if rand_chance < prob:
+                    accept_move = True
+
+            if accept_move == True:
+                working_state = next_guess
+                working_cost = next_cost
+                if working_cost < the_best_cost:
+                    the_best_cost = working_cost
+                    the_best_state = list(working_state)
+
+            temp = temp * cooling_rate
+
+        return the_best_state, the_best_cost, all_evals, all_states
